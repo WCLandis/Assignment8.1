e3c7afa50a493461f4f2302d0d26cb6fdb06c56f
WCLandis <95384790+WCLandis@users.noreply.github.com>
2021-12-10 14:07:09 -0500

    Update Landis_W_Assignment8.1.py

--- Landis_W_Assignment8.1.py
+++ Landis_W_Assignment8.1.py
@@ -1,15 +1,16 @@
 import os
+
 directory = input("What directory would you like to save your file into? \n")
 
-try:
-	os.mkdir(directory)
-
-except FileExistsError:
+if os.path.exists(directory) == True:
 	print(f'Working directory {directory} found ')
 
+elif os.path.exists(directory) == False:
+	print(f'Directory not found, creating: {directory} directory now.  ')
+	os.mkdir(directory)
+		
 file = input("What is the name of your file? \n")
 fullPath = directory+file
-
 
 name = input('What is your name? \n')
 address = input('What is your address? \n')
@@ -21,5 +22,5 @@ with open(fullPath, 'w+') as file_object:
 
 with open(fullPath, 'r') as file_object:
 	userAnswer = file_object.read()
-	print(f'You have saved the following information to {fullPath}: \n')
+	print(f'You have saved the following information to: {fullPath} \n')
 	print(userAnswer)	
