074f154a30f84d99168807d6067df0ad0dc4fff2
WCLandis <95384790+WCLandis@users.noreply.github.com>
2021-12-10 13:38:21 -0500

    Add files via upload

---
+++ Landis_W_Assignment8.1.py
@@ -0,0 +1,25 @@
+import os
+directory = input("What directory would you like to save your file into? \n")
+
+try:
+	os.mkdir(directory)
+
+except FileExistsError:
+	print(f'Working directory {directory} found ')
+
+file = input("What is the name of your file? \n")
+fullPath = directory+file
+
+
+name = input('What is your name? \n')
+address = input('What is your address? \n')
+phone = input('what is your phone number? \n')
+
+with open(fullPath, 'w+') as file_object:
+	file_object.write.format(f"{name}, {address}, {phone}")
+	
+
+with open(fullPath, 'r') as file_object:
+	userAnswer = file_object.read()
+	print(f'You have saved the following information to {fullPath}: \n')
+	print(userAnswer)	
